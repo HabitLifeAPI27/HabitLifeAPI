17f4a3e23adbf9085239755182a103507cbff85c
Meza27 <u202224016@upc.edu.pe>
2024-05-28 23:34:51 -0500

    feat:Documentación en swagger de la API

--- habitlife-api/src/main/java/com/habitlife/habitlifeapi/config/OpenAPIConfig.java
+++ habitlife-api/src/main/java/com/habitlife/habitlifeapi/config/OpenAPIConfig.java
@@ -1,4 +1,48 @@
 package com.habitlife.habitlifeapi.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class OpenAPIConfig {
+    @Value("${habitlife.openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        // Definir el servidor de desarrollo
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        // Información de contacto
+        Contact contact = new Contact();
+        contact.setEmail("u202224016@upc.edu.pe");
+        contact.setName("HabitLife");
+
+        // Licencia
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        // Información general de la API
+        Info info = new Info()
+                .title("HabitLife API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints.")
+                .license(mitLicense);
+
+
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
 }
